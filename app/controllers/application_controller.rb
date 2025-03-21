7eed0c724840ba8e83c01a915cae22d9428315e7
marcobeffa <marcobeffa@gmail.com>
2025-03-21 19:42:43 +0100

    fix 406

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  rescue_from ActionView::MissingTemplate, with: :fallback_for_old_browser
-
-  private
-
-  def fallback_for_old_browser
-    render template: "fallback/unsupported_browser", layout: "simpleposts", status: :not_acceptable
-  end
 end
