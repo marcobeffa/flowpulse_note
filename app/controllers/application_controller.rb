3834081e3194a758e0ead41f3fba0aeb280385cb
marcobeffa <marcobeffa@gmail.com>
2025-03-21 12:41:32 +0100

    fix 03 old browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,11 @@
 class ApplicationController < ActionController::Base
-  include Authentication
-
   allow_browser versions: :modern
 
   rescue_from ActionView::MissingTemplate, with: :fallback_for_old_browser
-  rescue_from ActionController::UnknownFormat, with: :fallback_for_old_browser
 
   private
 
-  def fallback_for_old_browser(exception)
-    if request.variant == :simple || exception.message.include?("Not Acceptable")
-      # fallback per browser non supportati
-      render template: "fallback/unsupported_browser", layout: "simpleposts", status: 406
-    else
-      raise exception # re-lancia se non � legato al browser
-    end
+  def fallback_for_old_browser
+    render template: "fallback/unsupported_browser", layout: "simpleposts", status: :not_acceptable
   end
 end
